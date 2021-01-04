9c7c7f5b44de2bb5503699f03c012b13270b5052
Robert Böhnke <robb@robb.is>
2021-01-04 11:11:18 +0100

    Inline reversedIfDebug

--- Sources/RBBJSON/RBBJSONQuery.swift
+++ Sources/RBBJSON/RBBJSONQuery.swift
@@ -231,7 +231,11 @@ extension RBBJSON {
                         return json
 
                     case .object(let object):
-                        stack.append(contentsOf: Array(object.values).sortedIfDebug.reversedIfDebug)
+                        #if DEBUG
+                        stack.append(contentsOf: Array(object.values).sortedIfDebug.reversed())
+                        #else
+                        stack.append(contentsOf: object.values)
+                        #endif
                         return json
                     }
                 }
@@ -324,18 +328,6 @@ internal extension Sequence where Element: Comparable {
     }
     #else
     var sortedIfDebug: Self {
-        self
-    }
-    #endif
-}
-
-internal extension BidirectionalCollection {
-    #if DEBUG
-    var reversedIfDebug: ReversedCollection<Self> {
-        reversed()
-    }
-    #else
-    var reversedIfDebug: Self {
         self
     }
     #endif
