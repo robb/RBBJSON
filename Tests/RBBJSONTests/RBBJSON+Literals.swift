010ac43d79b965252ea56c522972eeb93a472e8d
Robert Böhnke <robb@robb.is>
2025-08-21 20:58:33 +0200

    Fix warnings

--- Tests/RBBJSONTests/RBBJSON+Literals.swift
+++ Tests/RBBJSONTests/RBBJSON+Literals.swift
@@ -1,42 +1,42 @@
 import RBBJSON
 
-extension RBBJSON: ExpressibleByNilLiteral {
+extension RBBJSON: @retroactive ExpressibleByNilLiteral {
     public init(nilLiteral: ()) {
         self = .null
     }
 }
 
-extension RBBJSON: ExpressibleByBooleanLiteral {
+extension RBBJSON: @retroactive ExpressibleByBooleanLiteral {
     public init(booleanLiteral value: BooleanLiteralType) {
         self = .bool(value)
     }
 }
 
-extension RBBJSON: ExpressibleByFloatLiteral {
+extension RBBJSON: @retroactive ExpressibleByFloatLiteral {
     public init(floatLiteral value: Double) {
         self = .number(value)
     }
 }
 
-extension RBBJSON: ExpressibleByStringLiteral {
+extension RBBJSON: @retroactive ExpressibleByStringLiteral {
     public init(stringLiteral value: String) {
         self = .string(value)
     }
 }
 
-extension RBBJSON: ExpressibleByArrayLiteral {
+extension RBBJSON: @retroactive ExpressibleByArrayLiteral {
     public init(arrayLiteral elements: RBBJSON...) {
         self = .array(elements)
     }
 }
 
-extension RBBJSON: ExpressibleByIntegerLiteral {
+extension RBBJSON: @retroactive ExpressibleByIntegerLiteral {
     public init(integerLiteral value: Int) {
         self = .number(Double(value))
     }
 }
 
-extension RBBJSON: ExpressibleByDictionaryLiteral {
+extension RBBJSON: @retroactive ExpressibleByDictionaryLiteral {
     public init(dictionaryLiteral elements: (String, RBBJSON)...) {
         self = .object(Dictionary(elements) { a, _ in a })
     }
