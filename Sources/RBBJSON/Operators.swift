df538d98abc7d6d2d2687ddbad87bf62c30b97ed
Robert Böhnke <robb@robb.is>
2025-08-24 21:15:23 +0200

    Add ??= operator

--- Sources/RBBJSON/Operators.swift
+++ Sources/RBBJSON/Operators.swift
@@ -3,3 +3,9 @@ import Foundation
 public func ?? (lhs: consuming JSON, rhs: @autoclosure () throws -> JSON) rethrows -> JSON {
     lhs == .null ? (try rhs()) : lhs
 }
+
+infix operator ??= : AssignmentPrecedence
+
+public func ??= (lhs: inout JSON, rhs: @autoclosure () throws -> JSON) rethrows {
+    lhs = try lhs ?? rhs()
+}
